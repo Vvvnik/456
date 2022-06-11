300e3d482e451440057ef85a3c919128d03dd9cf
Vvvnik <vvv-nik@yandex.ru>
2022-06-11 14:58:46 +0300

    FIRST commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+import os
+import csv
+import json
+from datetime import datetime
+
+
+def collect_data():
+    pass
+
+
+def main():
+    print('hi')
+    collect_data()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
